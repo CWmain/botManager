59d585a0d473ce39588f748e0b2aca8cdb498b4b
CWmain <cameronwhite2478@gmail.com>
2025-03-15 22:12:21 +1100

    Added a backpack which increases player capcity but decreases movespeed. Added function that tracks added stats and a function to print it out

--- Objects/Bots/base_bot.gd
+++ Objects/Bots/base_bot.gd
@@ -7,8 +7,6 @@ extends Node2D
 @export var baseMoveSpeed: float = 20
 @export var baseRotationSpeed: float = 1
 
-
-
 @export_group("Equipped", "e_")
 @export var e_head: PackedScene
 @export var e_body: PackedScene
@@ -20,21 +18,22 @@ var curLegs: Equipment
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+## Stores the calculation breakdown of each element
+var modifierTracker: Dictionary[Enums.MODIFICATION, Array]
 
-var modifiers: Dictionary[Enums.MODIFICATION, Array]
 ## The total calculate movespeed
 var totalMoveSpeed: float = 0
 var totalRotationSpeed: float = 0
 
 func _ready() -> void:
 	sprite_2d.material.set_shader_parameter("original", c_original)
-	sprite_2d.material.set_shader_parameter("recolor", c_reColor)
-	
+	sprite_2d.material.set_shader_parameter("recolor", c_reColor)	
 	updateEquipment()
-	print(totalMoveSpeed)
+	print(modifierTracker)
+	print(Enums.MODIFICATION.find_key(0))
+	print_modifer_tracker()
 
-func _process(delta: float) -> void:
-	
+func _process(delta: float) -> void:	
 	var hor: float = Input.get_axis("ui_left", "ui_right")
 	rotation += hor*delta*totalRotationSpeed
 	var ver: float = Input.get_axis("ui_up", "ui_down")
@@ -46,6 +45,13 @@ func _process(delta: float) -> void:
 	var moveDirection: Vector2 = Vector2(0,ver).normalized().rotated(rotation)
 	position += moveDirection*totalMoveSpeed*delta
 
+## Function which prints out the data in modifierTracker in a clean way
+func print_modifer_tracker():
+	for mtype in modifierTracker:
+		print(Enums.MODIFICATION.find_key(mtype))
+		for pair in modifierTracker[mtype]:
+			print("	Added %f from equipment %s" % [pair[1], pair[0]])
+
 ## Called to recalculate totalStats (i.e moveSpeed)
 func updateEquipment():
 	var curEquipment: Equipment
@@ -55,7 +61,7 @@ func updateEquipment():
 		curHead = e_head.instantiate()
 		curHead.position = Vector2(0,-16)
 		add_child(curHead)
-		calculateTotalStats(curHead.statModifers)
+		calculateTotalStats(curHead)
 		
 	if e_body != null:
 		if curBody != null:
@@ -63,7 +69,7 @@ func updateEquipment():
 		curBody = e_body.instantiate()
 		curBody.position = Vector2(0,0)
 		add_child(curBody)
-		calculateTotalStats(curBody.statModifers)
+		calculateTotalStats(curBody)
 		
 	if e_legs != null:
 		if curLegs != null:
@@ -71,10 +77,11 @@ func updateEquipment():
 		curLegs = e_legs.instantiate()
 		curLegs.position = Vector2(0,16)
 		add_child(curLegs)
-		calculateTotalStats(curLegs.statModifers)
+		calculateTotalStats(curLegs)
 
-
-func calculateTotalStats(statChanges: Dictionary[Enums.MODIFICATION, float]):
+## Calculate the total stats and apply their passive effect onto totalStats
+func calculateTotalStats(equipment: Equipment):
+	var statChanges: Dictionary[Enums.MODIFICATION, float] = equipment.statModifers
 	totalMoveSpeed = baseMoveSpeed
 	totalRotationSpeed = baseRotationSpeed
 	for change in statChanges:
@@ -83,5 +90,11 @@ func calculateTotalStats(statChanges: Dictionary[Enums.MODIFICATION, float]):
 				totalMoveSpeed += statChanges[change]
 			Enums.MODIFICATION.ROTATIONSPEED:
 				totalRotationSpeed += statChanges[change]
+			_:
+				printerr("Unhandled Enum %s in %s" % [Enums.MODIFICATION.find_key(change), equipment.name])
+		if modifierTracker.has(change):
+			modifierTracker[change].append([equipment.name, statChanges[change]])
+		else:
+			modifierTracker[change] = [[equipment.name, statChanges[change]]]
+		
 		print(statChanges[change])
-	pass
