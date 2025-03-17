69aba6e15992c35d4b5cf8a1d52d1e2a02ba1b0f
CWmain <cameronwhite2478@gmail.com>
2025-03-17 15:54:56 +1100

    Basic miniview node made

--- UI/miniView/mini_view.gd
+++ UI/miniView/mini_view.gd
@@ -1,6 +1,26 @@
-extends ColorRect
+extends SubViewportContainer
 
-@export var toTrack: CharacterBody2D
+## The node 2d to center the camera on[br] 
+## The viewport is derived from toTack
+@export var toTrack: Node2D: 
+	set(value):
+		toTrack = value
+		updateViewportWorld2D()
+
+## The amount of zoom on the camera
+@export_range(0, 10, 0.1, "or_less", "or_greater") var cameraZoomScale: float = 1.0
+
+@onready var camera_2d: Camera2D = $SubViewport/Camera2D
+@onready var sub_viewport: SubViewport = $SubViewport
+
+func _ready() -> void:
+	updateViewportWorld2D()
 
 func _process(delta: float) -> void:
-	material.set_shader_parameter("toTrack", toTrack.global_position)
+	if toTrack != null:
+		camera_2d.position = toTrack.position
+		
+func updateViewportWorld2D():
+	if toTrack == null || sub_viewport == null:
+		return
+	sub_viewport.world_2d = toTrack.get_viewport().world_2d
