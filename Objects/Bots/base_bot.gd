44ef16bdd3e374c0dad27b737844d161bfa215a7
CWmain <cameronwhite2478@gmail.com>
2025-03-18 02:59:47 +1100

    Fixed weird issue of shader when two or more bots exists, now trying to fix issues of both taking same path

--- Objects/Bots/base_bot.gd
+++ Objects/Bots/base_bot.gd
@@ -34,9 +34,13 @@ var travelTo = false
 var modifierTracker: Dictionary[Enums.MODIFICATION, Array]
 
 func _ready() -> void:
+	# Have to duplicate the material locally otherwise an update in
+	# one instance updates all instances for recoloring
+	sprite_2d.material = sprite_2d.material.duplicate()
 	sprite_2d.material.set_shader_parameter("original", c_original)
-	sprite_2d.material.set_shader_parameter("recolor", c_reColor)	
+	sprite_2d.material.set_shader_parameter("recolor", c_reColor)
 	updateStatTotals()
+	print_current_equipment()
 	print_modifer_tracker()
 	path_finder.target_position = Vector2(150,100)
 	travelTo = true
@@ -44,33 +48,47 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("GoTo"):
 		path_finder.target_position = get_viewport().get_mouse_position()
+		print_debug(statTotals[Enums.MODIFICATION.MOVESPEED])
 		travelTo = true
 	if Input.is_action_just_pressed("BotCam"):
 		camera_2d.enabled = !camera_2d.enabled
+		camera_2d.make_current()
 
 func _physics_process(delta: float) -> void:
 	var nextPathPosition = path_finder.get_next_path_position()
 	rotation = (nextPathPosition - global_position).angle()+PI/2
 	
 	if travelTo:
-		curLegs.playAnimation()
+		if (curLegs != null):
+			curLegs.playAnimation()
 		var moveDirection: Vector2 = Vector2(0,-1).rotated(rotation)
 		velocity = moveDirection*statTotals[Enums.MODIFICATION.MOVESPEED]*delta
 	else:
-		curLegs.endAnimation()
+		if (curLegs != null):
+			curLegs.endAnimation()
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
 
 ## Function which prints out the data in modifierTracker in a clean way
 func print_modifer_tracker():
+	print("For %s" % name)
 	for mtype in modifierTracker:
 		print(Enums.MODIFICATION.find_key(mtype))
 		for pair in modifierTracker[mtype]:
 			print("	Added %f from equipment %s" % [pair[1], pair[0]])
 
+func print_current_equipment():
+	var empty: String = "NONE"
+	print("Equipment of ", name)
+	var headString: String = empty if curHead==null else curHead.EID
+	print("	Head: %s" % empty if curHead==null else curHead.EID)
+	print("	Body: ", empty if curBody==null else curBody.EID)
+	print("	Legs: ", empty if curLegs==null else curLegs.EID)
+
 ## Called to recalculate totalStats (i.e moveSpeed)
 func updateStatTotals():
+	statTotals = statBases.duplicate()
 	if e_head != null:
 		if curHead != null:
 			curHead.queue_free()
@@ -112,6 +130,11 @@ func calculateStatsFromEquipment(equipment: Equipment):
 		else:
 			modifierTracker[change] = [[equipment.name, statChanges[change]]]
 
+func wander()->void:
+	path_finder.target_position = position + Vector2(randf()*200-100,randf()*200-100)
+	travelTo = true
+	
 
 func _on_path_finder_navigation_finished() -> void:
 	travelTo = false
+	wander()
