424ce492a7408c987936edf329da78ad50599f33
CWmain <cameronwhite2478@gmail.com>
2025-03-18 15:46:17 +1100

    Slightly improved flee code

--- Objects/Bots/StateMachine/Flee/flee.gd
+++ Objects/Bots/StateMachine/Flee/flee.gd
@@ -1,9 +1,11 @@
 extends State
 
 @export var approach: State
-
+@export var fleeDistance: float = 100
 func enter()->void:
-	me.path_finder.target_position = Vector2(100,100)
+	# Get direction to foe
+	var fleeVector: Vector2 = me.position + me.position.direction_to(foe.position).rotated(PI/2)*fleeDistance
+	me.path_finder.target_position = fleeVector
 	me.travelTo = true
 	
 func update()->void:
