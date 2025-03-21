196ce1badb37011028b1a3edc3927ef9417b5392
CWmain <cameronwhite2478@gmail.com>
2025-03-21 20:08:02 +1100

    Add a raytrace to the front of bot to enable flee to detect if the bot if fleeing into a wall. If this is the case than the bot will attempt to go perpendicular to the foe in order to escape the corner

--- Objects/Bots/StateMachine/Flee/flee.gd
+++ Objects/Bots/StateMachine/Flee/flee.gd
@@ -2,9 +2,10 @@ extends State
 
 @export var approach: State
 @export var fleeDistance: float = 1000
+@export var rayCast: RayCast2D
 
 var oldFoePosition: Vector2
-
+var perpendicular: bool = false
 func enter()->void:
 	# Get direction to foe
 	var fleeVector: Vector2 = (foe.position + me.position.direction_to(foe.position).rotated(PI)*fleeDistance)
@@ -13,8 +14,20 @@ func enter()->void:
 	me.travelTo = true
 	
 func update()->void:
+	if me.path_finder.is_navigation_finished():
+		print("End of path")
+		perpendicular = false
+	
+	if rayCast.is_colliding() and foe.position != oldFoePosition:
+		print(rayCast.get_collider())
+		print("Ray cast colliding")
+		oldFoePosition = foe.position
+		var fleeVector: Vector2 = (foe.position + me.position.direction_to(foe.position).rotated(PI/2)*fleeDistance)
+		me.path_finder.target_position = fleeVector
+		me.travelTo = true
+		perpendicular = true
 	# Only update flee direction if foe has moved
-	if foe.position != oldFoePosition:
+	elif foe.position != oldFoePosition and !perpendicular:
 		var fleeVector: Vector2 = (foe.position + me.position.direction_to(foe.position).rotated(PI)*fleeDistance)
 		me.path_finder.target_position = fleeVector
 		oldFoePosition = foe.position
