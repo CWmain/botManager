c105193d49ed0de305c596b39c54de462115bf45
CWmain <cameronwhite2478@gmail.com>
2025-03-28 13:30:14 +1100

    Forgot to save xD

--- Objects/WorldObjectPlacement/world_object_placement.gd
+++ Objects/WorldObjectPlacement/world_object_placement.gd
@@ -6,10 +6,5 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("PlaceObject"):
 		var mousePos: Vector2 = get_viewport().get_mouse_position() if get_viewport().get_camera_2d()==null else get_viewport().get_camera_2d().get_global_mouse_position()
 		var mouseInMap: Vector2 = tml.local_to_map(mousePos)
-		var coords = Vector2i(6,4)
-		print(tml.get_cell_source_id(coords))
-		print(tml.get_cell_atlas_coords(coords))
-		print(tml.get_cell_alternative_tile(coords))
+
 		tml.set_cell(mouseInMap, 1, Vector2i.ZERO, 1)
-		
-		
