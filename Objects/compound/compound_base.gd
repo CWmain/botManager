44918c1a127b2f3160385f69d29f71c0b9f39d3e
CWmain <cameronwhite2478@gmail.com>
2025-04-02 16:58:32 +1100

    Messing with @tool to create a base case which can than have the needed nodes added

--- Objects/compound/compound_base.gd
+++ Objects/compound/compound_base.gd
@@ -2,3 +2,4 @@ extends Node2D
 class_name CompoundBase
 
 @export var statChange: Dictionary[compoundBot.STATS, float]
+@export var EID: String = "Default"
--- Objects/compound/wheels/wheels_base.gd
+++ Objects/compound/wheels/wheels_base.gd
@@ -1,1 +1,28 @@
+@tool
 extends CompoundBase
+class_name WheelBase
+
+var wheel_texture: Sprite2D = null
+
+func _ready() -> void:			
+	child_entered_tree.connect(_on_child_entered_tree)
+	child_exiting_tree.connect(_on_child_exiting_tree)
+	_get_configuration_warnings()
+	update_configuration_warnings()
+	
+	#wheel_texture.texture = sprite
+
+# Add any wheel related functions here
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = []
+	if wheel_texture == null:
+		warnings = ["Missing Sprite2D as child"]
+	return warnings
+
+func _on_child_entered_tree(node: Node) -> void:
+	if node is Sprite2D:
+		wheel_texture = node
+
+func _on_child_exiting_tree(node: Node) -> void:
+	if node is Sprite2D:
+		wheel_texture = null
